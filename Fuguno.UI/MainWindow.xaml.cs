b214c76515e46802db9bef30ba53526dffe3b02d
Adam Gilmore <adam.gilmore@outlook.com>
2013-07-17 00:08:18 +0100

    Added some charts to the Wpf app - requires WPF Toolkit to be installed

--- Fuguno.UI/MainWindow.xaml.cs
+++ Fuguno.UI/MainWindow.xaml.cs
@@ -11,6 +11,7 @@
     using System.Diagnostics;
     using System.Threading;
     using System.Windows;
+    using System.Windows.Controls.DataVisualization.Charting;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -19,6 +20,7 @@
     {
         IBuildInfoService _buildInfoService;
         IIterationInfoService _iterationInfoService;
+        IWorkItemStatsService _workItemStatsService;
         long _refreshInteralMilliseconds;
         BackgroundWorker _worker;
 
@@ -38,6 +40,11 @@
                 ConfigurationManager.AppSettings["TfsServerUri"],
                 ConfigurationManager.AppSettings["TfsCollectionName"],
                 ConfigurationManager.AppSettings["TfsProjectName"]);
+
+            _workItemStatsService = new WorkItemStatsService(
+                ConfigurationManager.AppSettings["TfsServerUri"],
+                ConfigurationManager.AppSettings["TfsCollectionName"]);
+
 
             var buildDefinitonNames = ConfigurationManager.AppSettings["TfsBuildDefinitionNames"].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
             foreach (var buildDefinitionName in buildDefinitonNames)
@@ -63,6 +70,27 @@
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
 
+                // Work Item stats
+                var tfsWorkItemStatsWorkItemType = ConfigurationManager.AppSettings["TfsWorkItemStatsWorkItemType"];
+                var tfsWorkItemStatsState = ConfigurationManager.AppSettings["TfsWorkItemStatsState"];
+                var tfsWorkItemStatsAreaPaths = ConfigurationManager.AppSettings["TfsWorkItemStatsAreaPaths"];
+                var areaPaths = tfsWorkItemStatsAreaPaths.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var activeBugsByPriority = _workItemStatsService.GetWorkItemCountByPriority(tfsWorkItemStatsWorkItemType, tfsWorkItemStatsState, areaPaths);
+
+                BugPriorityChart.Dispatcher.Invoke(new Action(delegate()
+                {
+                    ((ColumnSeries)BugPriorityChart.Series[0]).ItemsSource = activeBugsByPriority;
+                }));
+
+                var activeBugsByAssignedTo = _workItemStatsService.GetWorkItemCountByAssignedTo(tfsWorkItemStatsWorkItemType, tfsWorkItemStatsState, areaPaths);
+
+                BugAssignedToChart.Dispatcher.Invoke(new Action(delegate()
+                {
+                    ((BarSeries)BugAssignedToChart.Series[0]).ItemsSource = activeBugsByAssignedTo;
+                }));
+
+                // Iteration Info
                 var iterationInfo = _iterationInfoService.GetCurrentIterationInfo();
 
                 IterationInfoTextBlock.Dispatcher.Invoke(new Action(delegate()
@@ -70,6 +98,7 @@
                     IterationInfoTextBlock.Text = string.Format("{0} {1} days left", iterationInfo.Name, (iterationInfo.EndDate - DateTime.Now.Date).TotalDays);
                 }));
 
+                // Build Info
                 foreach (var buildInfoViewModel in _buildInfoViewModels)
                 {
                     var buildInfo = _buildInfoService.GetLatestBuildInfo(buildInfoViewModel.Name);
