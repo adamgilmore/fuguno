cce91ad20119a333a4b155d994aea737f409c2cc
Adam Gilmore <adam.gilmore@outlook.com>
2013-07-16 20:44:18 +0100

    Added WorkItemStatsService stub - going to return bug stats etc...

---
+++ Fuguno.Tfs/IWorkItemStatsService.cs
@@ -0,0 +1,7 @@
+﻿namespace Fuguno.Tfs
+{
+    public interface IWorkItemStatsService
+    {
+        //long GetWorkItemCount(string workItemType, string[] areaPaths, string[] states);
+    }
+}
---
+++ Fuguno.Tfs/WorkItemStatsService.cs
@@ -0,0 +1,37 @@
+﻿namespace Fuguno.Tfs
+{
+    using System.Text;
+
+    public class WorkItemStatsService : IWorkItemStatsService
+    {
+        //private const string WorkItemCountQueryTemplate = "SELECT [Title] FROM WorkItems WHERE [Work Item Type] = '{0}'";
+
+        //public long GetWorkItemCount(string workItemType, string[] areaPaths, string[] states)
+        //{
+        //    long workItemCount = 0;
+
+        //    var queryText = new StringBuilder(string.Format(WorkItemCountQueryTemplate, workItemType));
+
+        //    if (areaPaths != null & areaPaths.Length > 0)
+        //    {
+        //        if (areaPaths.Length > 1)
+        //            queryText.Append("(");
+
+        //        bool prefixWithOr = false;
+        //        foreach (var areaPath in areaPaths)
+        //        {
+        //            if (prefixWithOr == true)
+        //                queryText.Append(" OR ");
+        //            else
+        //                prefixWithOr = true;
+        //            queryText.AppendFormat("[Area Path] under '{0}'", areaPath.Trim());
+        //        }
+
+        //        if (areaPaths.Length > 1)
+        //            queryText.Append(")");
+        //    }
+
+        //    return workItemCount;
+        //}
+    }
+}
