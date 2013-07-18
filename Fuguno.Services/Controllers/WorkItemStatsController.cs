22991327b6665146fe7af99984fade18c38686a4
Adam Gilmore <adam.gilmore@outlook.com>
2013-07-18 22:09:11 +0100

    Added WebAPI WorkItemStatsController
    e.g. two methods that are used to get the data for the charts

---
+++ Fuguno.Services/Controllers/WorkItemStatsController.cs
@@ -0,0 +1,28 @@
+﻿namespace Fuguno.Services.Controllers
+{
+    using Fuguno.Tfs;
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    public class WorkItemStatsController : ApiController
+    {
+        [HttpGet, ActionName("assignedto")]
+        public IEnumerable<WorkItemStat> GetWorkItemCountByAssignedTo(string server, string collection, string workItemType, string state, string areaPaths)
+        {
+            var areaPathsArray = areaPaths.Split(new char[] { ',' });
+
+            var service = new WorkItemStatsService(server, collection);
+            return service.GetWorkItemCountByAssignedTo(workItemType, state, areaPathsArray);
+
+        }
+
+        [HttpGet, ActionName("priority")]
+        public IEnumerable<WorkItemStat> GetWorkItemCountByPriority(string server, string collection, string workItemType, string state, string areaPaths)
+        {
+            var areaPathsArray = areaPaths.Split(new char[] { ',' });
+
+            var service = new WorkItemStatsService(server, collection);
+            return service.GetWorkItemCountByPriority(workItemType, state, areaPathsArray);
+        }
+    }
+}
